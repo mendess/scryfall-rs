96537aeadf6311b980c41f78113c1ea4f0d0ae50
Stas <iamlolylol@gmail.com>
2020-01-21 12:17:41 +0200

    Upgrade dependency version of reqwest 0.9 => 0.10.1

--- tests/bulk_data.rs
+++ tests/bulk_data.rs
@@ -3,7 +3,7 @@ use scryfall::card::Card;
 #[test]
 fn parse_bulk_data() {
     let cards: Vec<Card> =
-        reqwest::get("https://archive.scryfall.com/json/scryfall-oracle-cards.json")
+        reqwest::blocking::get("https://archive.scryfall.com/json/scryfall-oracle-cards.json")
             .unwrap()
             .json()
             .unwrap();
