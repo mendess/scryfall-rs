91378970b01c6e3f79a55439cadda8432a5e234d
Michael Morgan <self@msmorgan.dev>
2021-02-19 21:26:38 -0500

    Comments for BulkDataFile fields.

--- src/bulk.rs
+++ src/bulk.rs
@@ -19,19 +19,57 @@ use crate::ruling::Ruling;
 use crate::uri::Uri;
 use crate::util::BULK_DATA_URL;
 
+/// Scryfall provides daily exports of our card data in bulk files. Each of
+/// these files is represented as a bulk_data object via the API. URLs for files
+/// change their timestamp each day, and can be fetched programmatically.
+///
+/// ## Please note:
+//
+// * Card objects in bulk data include price information, but prices should be considered
+//   dangerously stale after 24 hours. Only use bulk price information to track trends or provide a
+//   general estimate of card value. Prices are not updated frequently enough to power a storefront
+//   or sales system. You consume price information at your own risk.
+// * Updates to gameplay data (such as card names, Oracle text, mana costs, etc) are much less
+//   frequent. If you only need gameplay information, downloading card data once per week or right
+//   after set releases would most likely be sufficient.
+// * Every card type in every product is included, including planar cards, schemes, Vanguard cards,
+//   tokens, emblems, and funny cards. Make sure you’ve reviewed documentation for the Card type.
+//
+//
+// Bulk data is only collected once every 12 hours. You can use the card API methods to retrieve
+// fresh objects instead.
 #[derive(Deserialize, Debug, Clone)]
-#[allow(missing_docs)]
 pub struct BulkDataFile<T> {
+    /// A unique ID for this bulk item.
     pub id: Uuid,
+
+    /// The Scryfall API URI for this file.
     pub uri: Uri<BulkDataFile<T>>,
+
+    /// A computer-readable string for the kind of bulk item.
     #[serde(rename = "type")]
     pub bulk_type: String,
+
+    /// A human-readable name for this file.
     pub name: String,
+
+    /// A human-readable description for this file.
     pub description: String,
+
+    /// The URI that hosts this bulk file for fetching.
     pub download_uri: Uri<Vec<T>>,
+
+    /// The time when this file was last updated.
     pub updated_at: DateTime<Utc>,
+
+    /// The size of this file in integer bytes.
     pub compressed_size: usize,
+
+    /// The MIME type of this file.
     pub content_type: String,
+
+    /// The Content-Encoding encoding that will be used to transmit this file
+    /// when you download it.
     pub content_encoding: String,
 }
 
