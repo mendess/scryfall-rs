061ee74863fcb3d2daa4a9205f688c6b151a55b1
Michael Morgan <self@msmorgan.dev>
2021-02-19 21:55:38 -0500

    Comments for Layout.

--- src/card/layout.rs
+++ src/card/layout.rs
@@ -1,31 +1,58 @@
-//! Enum describing the various layouts a magic card can have.
 use serde::{Deserialize, Serialize};
 
-/// Enum describing the various layouts a magic card can have.
+/// The layout property categorizes the arrangement of card parts, faces, and
+/// other bounded regions on cards. The layout can be used to programmatically
+/// determine which other properties on a card you can expect.
 ///
-/// [Official docs](https://scryfall.com/docs/api/layouts)
+/// Specifically:
+///
+/// * Cards with the layouts split, flip, transform, and double_faced_token will
+///   always have a card_faces property describing the distinct faces.
+///
+/// * Cards with the layout meld will always have a related_cards property
+///   pointing to the other meld parts.
+///
+/// [Official docs](https://scryfall.com/docs/api/layouts#layout)
 #[derive(Serialize, Deserialize, Copy, Clone, Eq, PartialEq, Hash, Debug)]
 #[serde(rename_all = "snake_case")]
-#[allow(missing_docs)]
 #[non_exhaustive]
 pub enum Layout {
+    /// A standard Magic card with one face.
     Normal,
+    /// A split-faced card.
     Split,
+    /// Cards that invert vertically with the flip keyword.
     Flip,
+    /// Double-sided cards that transform.
     Transform,
-    Meld,
-    Leveler,
-    Saga,
-    Adventure,
-    Planar,
-    Scheme,
-    Vanguard,
-    Token,
-    DoubleFacedToken,
-    Emblem,
-    Augment,
-    Host,
-    ArtSeries,
-    DoubleSided,
+    /// Double-sided cards that can be played either-side.
     ModalDfc,
+    /// Cards with meld parts printed on the back.
+    Meld,
+    /// Cards with Level Up.
+    Leveler,
+    /// Saga-type cards.
+    Saga,
+    /// Cards with an Adventure spell part.
+    Adventure,
+    /// Plane and Phenomenon-type cards.
+    Planar,
+    /// Scheme-type cards.
+    Scheme,
+    /// Vanguard-type cards.
+    Vanguard,
+    /// Token cards.
+    Token,
+    /// Tokens with another token printed on the back.
+    DoubleFacedToken,
+    /// Emblem cards.
+    Emblem,
+    /// Cards with Augment.
+    Augment,
+    /// Host-type cards.
+    Host,
+    /// Art Series collectable double-faced cards.
+    ArtSeries,
+    /// A Magic card with two sides that are unrelated.
+    DoubleSided,
 }
